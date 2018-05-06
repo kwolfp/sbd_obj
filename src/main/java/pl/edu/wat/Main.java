8bce1d8d128da283e04b292cbe8fa37e6cfc4ce8
Kamil Przyborowski <kamil@przyborowski.ovh>
2018-05-06 13:26:53 +0200

    init commit

---
+++ src/main/java/pl/edu/wat/Main.java
@@ -0,0 +1,13 @@
+package pl.edu.wat;
+
+/**
+ * Created by Kamil Przyborowski
+ * Wojskowa Akademia Techniczna im. Jarosława Dąbrowskiego, Warszawa 2018.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
